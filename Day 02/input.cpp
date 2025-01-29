1c723fb5e2b5d94d101b2ecbf7f093545dddf406
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-01-29 20:43:05 +0530

    Input Format

---
+++ Day 02/input.cpp
@@ -0,0 +1,12 @@
+// Input Format
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout << "Enter your age : " << endl;
+    cin >> age;
+    cout << "Your age is : " << age << endl;
+    return 0;
+}
