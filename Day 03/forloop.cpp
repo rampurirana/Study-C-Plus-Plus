e46fc0ac7509ac1243c664ef2da4344509509cc8
Rampravesh Rana <rampraveshrana1435@gmail.com>
2025-02-10 20:57:45 +0530

    For loop

---
+++ Day 03/forloop.cpp
@@ -0,0 +1,15 @@
+// Loops are used to execute a block of code repeatedly as long as certain comsdition is true
+
+// for loop : The for loop is used when i know how many times to execute a statement or a block of code
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "Number : " << i << endl;
+    }
+    return 0;
+}
